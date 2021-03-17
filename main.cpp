a8b087dea1887b1aef094912bd422c40552a93b8
Amit-Dey <amitdey9020@gmail.com>
2021-03-17 11:04:38 +0600

    My first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{   string name;
+    cin>>name;
+    
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"Hello "<<name<<endl<<"How is it going..?"<<endl;
+    }
+    
+    return 0;
+}
